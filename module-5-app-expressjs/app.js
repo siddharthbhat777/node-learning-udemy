b6a14c11ab564b803b2eec806d519c3494c98821
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-03 17:30:54 +0530

    404 page not found

--- module-5-app-expressjs/app.js
+++ module-5-app-expressjs/app.js
@@ -15,6 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found.</h1>")
+});
+
 /*
 app.use((req, res, next)=>{ // 'app.use()' helps you to set up middlewares
     console.log("In the middleware!");

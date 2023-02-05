19d12777a8854cb5bec5c683dd198df057e208ea
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-06 01:35:59 +0530

    post cart done

--- module-9-app-dynamic-routing/models/product.js
+++ module-9-app-dynamic-routing/models/product.js
@@ -27,6 +27,7 @@ module.exports = class Product {
 
   save() {
     this.id = Math.random().toString();
+    console.log(this.id);
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {

d10bf7bee2b7d725fe99b90a21ce99a6cccd07f1
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-09 23:10:34 +0530

    fetching related products

--- module-11-app-sequelize/controllers/admin.js
+++ module-11-app-sequelize/controllers/admin.js
@@ -33,7 +33,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then((product) => {
+  Product.findByPk(prodId)
+  req.user.getProducts({ where: { id: prodId } }).then((products) => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -69,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
+  req.user.getProducts().then((products) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',

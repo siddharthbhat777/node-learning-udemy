07b020ea7c880f195e7d6a3def1a4521d22996db
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-10 19:53:51 +0530

    module 12 initialized

---
+++ module-12-app-mongodb/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-learning', 'root', 'Sid@2023', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
+
+module.exports = sequelize;

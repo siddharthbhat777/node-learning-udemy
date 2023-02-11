2c4e59ead70f1d452a04e8a86ebe1c092b39de4e
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-11 16:33:24 +0530

    add to cart POST done

--- module-12-app-mongodb/app.js
+++ module-12-app-mongodb/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     User.findById("63e748132ebbeafac28dc349").then((user) => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     }).catch((err) => {
         console.log(err);
--- module-12-app-mongodb/controllers/shop.js
+++ module-12-app-mongodb/controllers/shop.js
@@ -55,29 +55,36 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  let fetchedCart;
-  let newQuantity = 1;
-  req.user.getCart().then((cart) => {
-    fetchedCart = cart;
-    return cart.getProducts({ where: { id: prodId } });
-  }).then(products => {
-    let product;
-    if (products.length > 0) {
-      product = products[0];
-    }
-    if (product) { // if the product is already in cart then increase its quantity
-      const oldQuantity = product.cartItem.quantity;
-      newQuantity = oldQuantity + 1;
-      return product;
-    }
-    return Product.findByPk(prodId); // adding new product
-  }).then(product => {
-    return fetchedCart.addProduct(product, { through: { quantity: newQuantity } }); // addProduct() is magic method by sequelize for many to many relationships
-  }).then(() => {
-    res.redirect('/cart');
+  Product.findById(prodId).then((product) => {
+    return req.user.addToCart(product);
+  }).then(result => {
+    console.log(result);
   }).catch((err) => {
     console.log(err);
   });
+  // let fetchedCart;
+  // let newQuantity = 1;
+  // req.user.getCart().then((cart) => {
+  //   fetchedCart = cart;
+  //   return cart.getProducts({ where: { id: prodId } });
+  // }).then(products => {
+  //   let product;
+  //   if (products.length > 0) {
+  //     product = products[0];
+  //   }
+  //   if (product) { // if the product is already in cart then increase its quantity
+  //     const oldQuantity = product.cartItem.quantity;
+  //     newQuantity = oldQuantity + 1;
+  //     return product;
+  //   }
+  //   return Product.findByPk(prodId); // adding new product
+  // }).then(product => {
+  //   return fetchedCart.addProduct(product, { through: { quantity: newQuantity } }); // addProduct() is magic method by sequelize for many to many relationships
+  // }).then(() => {
+  //   res.redirect('/cart');
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {

496a7e260eb13f7fd18bea0fdd24581bc599a1ea
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-11 17:51:54 +0530

    delete cart item done

--- module-12-app-mongodb/controllers/shop.js
+++ module-12-app-mongodb/controllers/shop.js
@@ -59,39 +59,11 @@ exports.postCart = (req, res, next) => {
   }).catch((err) => {
     console.log(err);
   });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user.getCart().then((cart) => {
-  //   fetchedCart = cart;
-  //   return cart.getProducts({ where: { id: prodId } });
-  // }).then(products => {
-  //   let product;
-  //   if (products.length > 0) {
-  //     product = products[0];
-  //   }
-  //   if (product) { // if the product is already in cart then increase its quantity
-  //     const oldQuantity = product.cartItem.quantity;
-  //     newQuantity = oldQuantity + 1;
-  //     return product;
-  //   }
-  //   return Product.findByPk(prodId); // adding new product
-  // }).then(product => {
-  //   return fetchedCart.addProduct(product, { through: { quantity: newQuantity } }); // addProduct() is magic method by sequelize for many to many relationships
-  // }).then(() => {
-  //   res.redirect('/cart');
-  // }).catch((err) => {
-  //   console.log(err);
-  // });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.getCart().then((cart) => {
-    return cart.getProducts({ where: { id: prodId } });
-  }).then(products => {
-    const product = products[0];
-    return product.cartItem.destroy();
-  }).then(result => {
+  req.user.deleteItemFromCart(prodId).then(result => {
     res.redirect('/cart');
   }).catch((err) => {
     console.log(err);

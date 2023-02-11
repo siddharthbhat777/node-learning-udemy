49ae5e334431a0531b9712d79a167b1ae7ff5a7a
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-11 13:21:10 +0530

    created user model

--- module-12-app-mongodb/app.js
+++ module-12-app-mongodb/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,6 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
+    User.findById("63e748132ebbeafac28dc349").then((user) => {
+        req.user = user;
+        next();
+    }).catch((err) => {
+        console.log(err);
+    });
     next();
 });
 

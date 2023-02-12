3298560d7b4e41a76b12b2330e51aaf3743d709c
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-12 22:55:57 +0530

    module 14 initialized

---
+++ module-14-app-session-and-cookies/controllers/shop.js
@@ -0,0 +1,110 @@
+const Order = require('../models/order');
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  // find() is mmongoose method same as fetchAll()
+  // If data is large then use find().cursor(), which is called pagination
+  Product.find().then((products) => {
+    console.log(products);
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId).then(product => { // mongoose also has a 'findById()' method
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  }).catch((err) => {
+    console.log(err);
+  });;
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.find().then((products) => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user.populate('cart.items.productId').then((user) => {
+    const products = user.cart.items;
+    res.render('shop/cart', {
+      path: '/cart',
+      pageTitle: 'Your Cart',
+      products: products
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId).then((product) => {
+    return req.user.addToCart(product);
+  }).then(result => {
+    console.log(result);
+    res.redirect('/cart');
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.removeFromCart(prodId).then(result => {
+    res.redirect('/cart');
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user.populate('cart.items.productId').then((user) => {
+    const products = user.cart.items.map(i => {
+      return { quantity: i.quantity, product: { ...i.productId._doc } };// doc returns whole document instead of just id
+    });
+    const order = new Order({
+      user: {
+        name: req.user.name,
+        userId: req.user
+      },
+      products: products
+    });
+    return order.save();
+  }).then(result => {
+    return req.user.clearCart();
+  }).then(() => {
+    res.redirect('/orders');
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ 'user.userId': req.user._id }).then((orders) => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+};
---
+++ module-14-app-session-and-cookies/models/product.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // the collection of user id you are referring
+      required: true
+    }
+});
+
+/*
+- 'Product' is what you will call it all over your project. It's just like alias.
+- And normally the collection name in database will be displayed as 'products', as it converts
+  'Product' to all small case and in plural form, the reason it puts name as 'products'
+*/
+
+module.exports = mongoose.model('Product', productSchema);
+
+/*
+Why to create a Schema in NO-SQL?
+- We don't actually need any schema when we are creating models with a default method i.e. using 
+  mongoDB Driver(module-12)
+- As now we are using mongoose which will manage most of the stuffs for us, and will let us focus
+  more over our data. To do so, mongoose atleast needs to know how our data actually looks like,
+  so for that reason we define a structure of our data for mongoose.
+- This dosen't mean our data is not flexible, we can still fill half of the data and keep other data
+  as blank (which is called partial tolerance) unless we don't mention required attribute there.
+*/
+
+// const mongodb = require('mongodb');
+
+// class Product {
+//   constructor(title, price, description, imageUrl, id, userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this._id = id? new mongodb.ObjectId(id) : null;
+//     this.userId = userId;
+//   }
+
+//   save() {
+//     const db = getDb();
+//     let dbOp;
+//     if (this._id) {
+//       dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this });
+//     } else {
+//       dbOp = db.collection('products').insertOne(this);
+//     }
+//     return dbOp.then((result) => {
+//       console.log(result);
+//     }).catch((err) => {
+//       console.log(err);
+//     });
+//   }
+
+//   static fetchAll() {
+//     const db = getDb();
+//     return db.collection('products').find().toArray().then((products) => {
+//       console.log(products);
+//       return products;
+//     }).catch((err) => {
+//       console.log(err);
+//     });
+//     /*
+//     toArray(): takes all of your data onto an array to display them at once
+//     - only use it if you know your data is limited upto hundreds
+//     - if the data is huge then prefer pagination
+//     */
+//   }
+
+//   static findById(prodId) {
+//     const db = getDb();
+//     return db.collection('products').find({ _id: new mongodb.ObjectId(prodId) }).next().then((product) => {
+//       console.log(product);
+//       return product;
+//     }).catch((err) => {
+//       console.log(err);
+//     });
+//   }
+
+//   static deleteById(prodId) {
+//     const db = getDb();
+//     return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) }).then((result) => {
+//       console.log("Deleted");
+//     }).catch((err) => {
+//       console.log(err);
+//     });
+//   }
+// }
+
+// module.exports = Product;

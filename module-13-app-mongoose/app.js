f5934f709aae194dc6fb2a195e05562c025bb3eb
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-12 12:48:29 +0530

    adding user

--- module-13-app-mongoose/app.js
+++ module-13-app-mongoose/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById("63e748132ebbeafac28dc349").then((user) => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     }).catch((err) => {
-//         console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+    User.findById('63e892554839f9ee4f15ef3e').then((user) => {
+        req.user = user;
+        next();
+    }).catch((err) => {
+        console.log(err);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +33,20 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://node-learning-app:SidB2023@nodelearningnosql.81hjxqp.mongodb.net/shop?retryWrites=true&w=majority').then((result) => {
+    User.findOne().then((user) => {
+        if (!user) {
+            const user = new User({
+                name: 'Siddharth',
+                email: 'sid123@gmail.com',
+                cart: {
+                    items: []
+                }
+            });
+            user.save();
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
     app.listen(3000);
     console.log("Connected!");
 }).catch((err) => {

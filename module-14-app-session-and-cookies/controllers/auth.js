f00d022eb4b2e66847ee1e1a9dbd825f5b1aadec
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-13 06:58:36 +0530

    using session for same cookie operation, but session is secured here

--- module-14-app-session-and-cookies/controllers/auth.js
+++ module-14-app-session-and-cookies/controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('Cookie').trim().split('=')[1];
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -8,6 +9,11 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10'); // cookie will expire in 10 secs
+    // res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10'); // cookie will expire in 10 secs
+    /*
+    cookie value in browser: true
+    session value in browser: s%3A7s5HONae9ybO_majlB411qjjjuZSBL77.7IJOrJ9dW3mguQp2ngjSYoY%2BejPqNXijcGsSS5HbdCI
+    */
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };

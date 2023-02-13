15313442e1ab9d32609c96b41da400832fde9747
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-13 21:50:25 +0530

    login done

--- module-15-app-authentication/controllers/auth.js
+++ module-15-app-authentication/controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs'); // package which encrypts password
 
 const User = require('../models/user');
 
@@ -19,13 +19,26 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {
+            console.log(err);
+            return res.redirect('/');
+          });
+        }
+        res.redirect('/login');
+      }).catch((err) => {
         console.log(err);
-        res.redirect('/');
+        res.redirect('/login');
       });
     })
     .catch(err => console.log(err));

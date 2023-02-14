631e0a6c844a6cdd61f961f605ddf98df42905da
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-15 00:08:51 +0530

    improved authorization
    In the last commit, the user can only see his own products in admin and can also edit it.
    - Whereas, there was a flaw i.e. if the user know the id of the product he could be able to put it in url and modify the product.
    - To avoid such thing we improved authorization by restricting unauthorized user from modifying product by checking the userId assigned to a product.
    - With this if unauthorized put product id in url, he will be able to view it but will not able to modify it.

--- module-17-advanced-authentication/controllers/admin.js
+++ module-17-advanced-authentication/controllers/admin.js
@@ -60,19 +60,19 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(result => {
+  Product.findById(prodId).then(product => {
+    if (product.userId.toString() !== req.user._id.toString()) {
+      return res.redirect('/');
+    }
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    return product.save().then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
+  }).catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
@@ -92,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');

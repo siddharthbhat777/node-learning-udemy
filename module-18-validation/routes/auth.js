8e2b6b7c52a2c398f1ef9121da6e921c25ddf00d
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-15 23:46:36 +0530

    adding multiple validation checks

--- module-18-validation/routes/auth.js
+++ module-18-validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check'); // performing destructuring
+const { check, body } = require('express-validator/check'); // performing destructuring
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,22 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, { req }) => {
+                if (value === 'test@test.com') {
+                    throw new Error('This email address is forbidden.')
+                }
+                return true;
+            }),
+        body('password', 'Please enter a password with only numbers and text and atleast 5 characters.')
+        .isLength({min: 5}).isAlphanumeric()
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 

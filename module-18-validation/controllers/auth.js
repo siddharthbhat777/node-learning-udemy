5e945555e53c82f9cc555cb55d5a8de225f87dec
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-15 23:27:32 +0530

    added an email validator

--- module-18-validation/controllers/auth.js
+++ module-18-validation/controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto'); // in-built library of node js helps in creati
 const bcrypt = require('bcryptjs'); // package which encrypts password
 const nodemailer = require('nodemailer');
 // const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -80,6 +81,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
   User.findOne({ email: email }).then((userDoc) => {
     // checking if user already exist
     if (userDoc) {

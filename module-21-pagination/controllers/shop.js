93be1e5361a0ba2cd9bfaeaf8b872cad3b619598
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-21 23:21:39 +0530

    added pagination to products page as well

--- module-21-pagination/controllers/shop.js
+++ module-21-pagination/controllers/shop.js
@@ -6,14 +6,25 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
-  Product.find().then(products => {
+  const page = +req.query.page || 1; // 'page' is the name of query parameter you gave
+  let totalItems;
+  Product.find().countDocuments().then((numProducts) => {
+    totalItems = numProducts;
+    return Product.find().skip((page - 1) * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE);
+  }).then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products'
+      path: '/products',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   }).catch(err => {
     const error = new Error(err);

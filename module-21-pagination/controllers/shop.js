cba6ba8c4dfd731c6a3a7f08797032120de036a2
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-21 20:44:11 +0530

    removed unnecessary

--- module-21-pagination/controllers/shop.js
+++ module-21-pagination/controllers/shop.js
@@ -8,7 +8,6 @@ const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find().then(products => {
-    console.log(products);
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',

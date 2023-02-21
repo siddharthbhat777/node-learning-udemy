6af1418c15370700975b5d450628bc3775d4ec10
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-21 16:59:39 +0530

    adding data in pdf

--- module-20-file-updoad-and-download/controllers/shop.js
+++ module-20-file-updoad-and-download/controllers/shop.js
@@ -149,7 +149,17 @@ exports.getInvoice = (req, res, next) => {
     pdfDoc.pipe(res);
 
     // content we want in pdf
-    pdfDoc.text('Hello World!');
+    pdfDoc.fontSize(26).text('Invoice', {
+      underline: true
+    });
+    pdfDoc.text('-----------------------------');
+    let totalPrice = 0;
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc.fontSize(14).text(prod.product.title + '-' + prod.quantity + ' x ' + '$' + prod.product.price)
+    });
+    pdfDoc.text('------------------------------');
+    pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
     pdfDoc.end();
     /*

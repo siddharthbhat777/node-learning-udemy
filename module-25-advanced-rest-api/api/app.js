c884ba26ea0343114bc66bc0e3b00532c6cda1b8
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-23 00:08:38 +0530

    connected mongodb and created post model

--- module-25-advanced-rest-api/api/app.js
+++ module-25-advanced-rest-api/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -19,4 +20,8 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://node-social-app:SidB2023@nodesocial.4wrvdbu.mongodb.net/messages?retryWrites=true&w=majority').then((result) => {
+    app.listen(8080);
+}).catch((err) => {
+    console.log(err);
+});

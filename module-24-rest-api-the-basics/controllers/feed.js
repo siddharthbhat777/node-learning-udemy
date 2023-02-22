b14c4247392c8ae76fc8ddd5fdb2376983a64262
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-22 17:45:33 +0530

    CORS error solved and module 24 done

--- module-24-rest-api-the-basics/controllers/feed.js
+++ module-24-rest-api-the-basics/controllers/feed.js
@@ -1,1 +1,15 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({ // 200: success
+        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({ // 201: success and resource is created
+        message: 'Post created successfully!',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    });
+};

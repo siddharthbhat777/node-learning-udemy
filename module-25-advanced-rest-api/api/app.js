25c241cd0c0275f58607691037707c613cbf613b
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-23 18:14:45 +0530

    created auth controller

--- module-25-advanced-rest-api/api/app.js
+++ module-25-advanced-rest-api/api/app.js
@@ -48,7 +48,8 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500; // setting default value as 500 if undefined
     const message = error.message;
-    res.status(status).json({ message: message });
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect('mongodb+srv://node-social-app:SidB2023@nodesocial.4wrvdbu.mongodb.net/messages?retryWrites=true&w=majority').then((result) => {

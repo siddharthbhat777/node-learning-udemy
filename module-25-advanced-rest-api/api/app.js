e8db5fe35f183cdff8cd14bf28cef15b2c7f75a6
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-23 15:27:27 +0530

    update done

--- module-25-advanced-rest-api/api/app.js
+++ module-25-advanced-rest-api/api/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
@@ -15,7 +14,7 @@ const fileStorage = multer.diskStorage({
         cb(null, 'images')
     },
     filename: (req, file, cb) => {
-        cb(null, uuidv4())
+        cb(null, new Date().getTime() + '-' + file.originalname)
     }
 });
 

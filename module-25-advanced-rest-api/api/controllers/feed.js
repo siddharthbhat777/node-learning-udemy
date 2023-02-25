126d30a049b74057a73da4a0ed350ea61bfb31ad
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-25 13:09:58 +0530

    authorization user check on update and delete

--- module-25-advanced-rest-api/api/controllers/feed.js
+++ module-25-advanced-rest-api/api/controllers/feed.js
@@ -109,6 +109,11 @@ exports.updatePost = (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
         if (imageUrl !== post.imageUrl) {
             clearImage(post.imageUrl);
         }
@@ -134,7 +139,11 @@ exports.deletePost = (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
-        // check logged in user
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
         clearImage(post.imageUrl);
         return Post.findByIdAndRemove(postId);
     }).then((result) => {

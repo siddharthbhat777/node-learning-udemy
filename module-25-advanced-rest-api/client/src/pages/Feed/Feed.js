1f88f24ca5014b87211c69b007a02b3511be31cd
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-25 12:33:51 +0530

    added auth to other routes

--- module-25-advanced-rest-api/client/src/pages/Feed/Feed.js
+++ module-25-advanced-rest-api/client/src/pages/Feed/Feed.js
@@ -127,7 +127,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     }).then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
@@ -179,7 +182,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {

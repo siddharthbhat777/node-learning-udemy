9037cf0abe063b5c10d3242ef2433b3b29038813
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-25 14:39:14 +0530

    status update done

--- module-25-advanced-rest-api/api/controllers/auth.js
+++ module-25-advanced-rest-api/api/controllers/auth.js
@@ -54,11 +54,54 @@ exports.login = (req, res, next) => {
             {
                 email: loadedUser.email,
                 userId: loadedUser._id.toString()
-            }, 
-            'somesupersecretsecret', 
+            },
+            'somesupersecretsecret',
             { expiresIn: '1h' }
         );
         res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    }).catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.getUserStatus = (req, res, next) => {
+    User.findById(req.userId).then((user) => {
+        if (!user) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({ status: user.status });
+    }).catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.updateUserStatus = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed.');
+        error.statusCode = 422;
+        error.data = error.array();
+        throw error;
+    }
+    const newStatus = req.body.status;
+    User.findById(req.userId).then((user) => {
+        if (!user) {
+            const error = new Error('User not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        user.status = newStatus;
+        return user.save();
+    }).then((result) => {
+        res.status(200).json({ message: 'User updated.' });
     }).catch((err) => {
         if (!err.statusCode) {
             err.statusCode = 500;
--- module-25-advanced-rest-api/api/routes/auth.js
+++ module-25-advanced-rest-api/api/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { body } = require('express-validator/check');
+
 const User = require('../models/user');
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -22,6 +24,14 @@ router.put('/signup',
     ], authController.signup
 );
 
-router.post('/login', authController.login)
+router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch('/status', isAuth,
+    [
+        body('status').trim().not().isEmpty()
+    ], authController.updateUserStatus
+);
 
 module.exports = router;

14200befc09fd66f21bfe5c7fe9eed9e249d68db
Siddharth Bhat <dbgt4531@gmail.com>
2023-02-26 23:16:27 +0530

    connection between client and web socket server

--- module-27-web-socket/api/app.js
+++ module-27-web-socket/api/app.js
@@ -55,11 +55,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose.connect('mongodb+srv://node-social-app:SidB2023@nodesocial.4wrvdbu.mongodb.net/messages?retryWrites=true&w=majority').then((result) => {
-}).then((result) => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server);
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
-        console.log('Client connected');
+        console.log('Client Connected');
     });
 }).catch((err) => {
     console.log(err);
---
+++ module-27-web-socket/api/socket.js
@@ -0,0 +1,13 @@
+let io;
+
+module.exports = {
+    init: httpServer => {
+        io = require('socket.io')(httpServer, {
+            cors: {
+                origin: "http://localhost:3000", //your client URL
+                methods: ["GET", "POST"]
+            }
+        });
+        return io;
+    }
+};

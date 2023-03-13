8fec942af957ec7347f5e510f8fecbe597737926
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-13 13:35:30 +0530

    testing asynchronous code

---
+++ module-30-testing/api/test/auth-controller.js
@@ -0,0 +1,26 @@
+const expect = require("chai").expect;
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller - Login', function () {
+    it('should throw an error with code 500 if accessing the database fails', function (done) {
+        sinon.stub(User, 'findOne');
+        User.findOne.throws();
+
+        const req = {
+            body: {
+                email: 'test@test.com',
+                password: 'tester'
+            }
+        }
+        
+        AuthController.login(req, {}, () => {}).then(result => {
+            expect(result).to.be.an('error');
+            expect(result).to.have.property('statusCode', 500); // testing if error code 500 is generated
+            done(); // to wait for asynchronous code to finish up
+        });
+        User.findOne.restore();
+    });
+});

173764450b67523a7183a47073a650f20a81a59d
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-16 14:32:21 +0530

    using unions

--- module-33-typescript/app.js
+++ module-33-typescript/app.js
@@ -1,13 +1,20 @@
-"use strict";
-const num1Element = document.getElementById('num1');
-const num2Element = document.getElementById('num2');
-const buttonElement = document.querySelector('button');
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-buttonElement.addEventListener('click', () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const result = add(+num1, +num2);
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    var stringResult = add(num1, num2);
     console.log(result);
+    console.log(stringResult);
 });

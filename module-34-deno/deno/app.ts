1774151851464dec3802c2a13e56dac00bcbc3ba
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-19 21:17:45 +0530

    fixed cors

--- module-34-deno/deno/app.ts
+++ module-34-deno/deno/app.ts
@@ -4,8 +4,15 @@ import todosRoutes from "./routes/todos.ts";
 
 const app = new Application();
 
-app.use(async (ctx, next) => {
+app.use( async (ctx, next) => {
   console.log('Middleware!');
+  await next();
+});
+
+app.use( async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
   await next();
 });
 

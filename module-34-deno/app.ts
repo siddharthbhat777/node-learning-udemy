bd0cc1c0c47cf623cd823b3e5633fb0cb28492bc
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-19 17:52:05 +0530

    using oak

--- module-34-deno/app.ts
+++ module-34-deno/app.ts
@@ -1,10 +1,20 @@
-import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
+// import { serve } from "https://deno.land/std@0.180.0/http/server.ts";
+
+import { Application } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
 
 // New code of latest versions
-async function reqHandler(req: Request) {
+/* async function reqHandler(req: Request) {
   return new Response("Hello world");
 }
-serve(reqHandler, { port: 5000 });
+serve(reqHandler, { port: 5000 }); */
 
 // Old code, don't work now
 /* const server = serve({ port: 5000 });

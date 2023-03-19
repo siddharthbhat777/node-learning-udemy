bb353e84bed968cb7f3559f0f519168a99244f8d
Siddharth Bhat <dbgt4531@gmail.com>
2023-03-19 20:11:46 +0530

    denp rest api done

---
+++ module-34-deno/deno/app.ts
@@ -0,0 +1,15 @@
+import { Application } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+
+import todosRoutes from "./routes/todos.ts";
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+  console.log('Middleware!');
+  await next();
+});
+
+app.use(todosRoutes.routes());
+app.use(todosRoutes.allowedMethods());
+
+await app.listen({ port: 3000 });
